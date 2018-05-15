b57169a5ff94539c8bcb66296175eb01af124698
申鹏飞 <spf@arthas.me>
2018-05-15 11:00:19 +0800

    4.3 blending

--- src/04 Advanced WebGL/03 Blending/index.js
+++ src/04 Advanced WebGL/03 Blending/index.js
@@ -1,20 +1,20 @@
 import * as glm from 'gl-matrix'
 import * as OBJ from 'webgl-obj-loader'
 
-import Camera from './camera'
-import Shader from './shader'
+import Camera from './../../camera'
+import Shader from './../../shader'
 
-import vsSource from './03 Model Loading/model_loading.vs'
-import fsSource from './03 Model Loading/model_loading.fs'
-import alphaTexFsSource from './04 Advanced WebGL/03 Blending/alpha_tex.fs'
+import vsSource from './../../03 Model Loading/model_loading.vs'
+import fsSource from './../../03 Model Loading/model_loading.fs'
+import alphaTexFsSource from './alpha_tex.fs'
 
-import suzanneObj from './assets/suzanne.obj'
-import planeObj from './assets/plane.obj'
-import sphereObj from './assets/sphere.obj'
+import suzanneObj from './../../assets/suzanne.obj'
+import planeObj from './../../assets/plane.obj'
+import sphereObj from './../../assets/sphere.obj'
 
-import wood from './assets/WoodFineDark004_COL_3K.jpg'
-import windowSrc from './assets/window.png'
-import grassSrc from './assets/grass.png'
+import wood from './../../assets/WoodFineDark004_COL_3K.jpg'
+import windowSrc from './../../assets/window.png'
+import grassSrc from './../../assets/grass.png'
 
 async function init(){
     document.body.style.margin = 0
